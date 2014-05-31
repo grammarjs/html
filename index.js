b568fecbd0e7f0633c448cdd7203327d568f4ed2
Lance Pollard <lancejpollard@gmail.com>
2014-05-31 17:07:54 +1000

    add better way to undo stack for tracking current expression.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 var Grammar = require('grammarjs-grammar');
 var grammar = new Grammar('markup');
 var expression = grammar.expression;
+var slice = [].slice;
 
 /**
  * Expose `grammar`.
@@ -18,231 +19,286 @@ module.exports = grammar;
  */
 
 expression('markup')
-  .match(':doctype?', ':tag*', function($1, $2){
-    var tokens = [];
-    if ($1) tokens = tokens.concat($1);
-    if ($2) {
-      $2.forEach(function(x){
-        tokens = tokens.concat(x);
-      });
-    }
-    return tokens;
-  })
-  //.match(':doctype?', ':tags', args);
-
-/**
- * Top of HTML file.
- */
-
-expression('prolog')
-  .match(/<\?.+?\?>/);
-
-/**
- * Doctype.
- */
-
-expression('doctype')
-  .match('<!', ':doctype-name', ':ws', /[a-z]+/, '>', function($1, $2, $3, $4, $5){
-    return [
-      token('punctuation', $1),
-      token('doctype', $2),
-      $3,
-      token('meta', $4), 
-      $5
-    ];
-  });
-
-/**
- * Doctype tag name.
- */
-
-expression('doctype-name')
-  .match('doctype', value)
-  .match('DOCTYPE', value);
+  .match(':tag.begin', passthrough);
 
 /**
  * Tag.
  */
 
 expression('tag')
-  //.match(':inline-tag', value)
-  .match(':open-tag', ':tag?', ':close-tag', ':ws', function($1, $2, $3, $4){
-    if ($2) $1.push($2);
-    $1 = $1.concat($3);
-    if ($4) $1.push($4);
-    return $1;
-  })
-  .match(':text', value)
-  .match(':comment', value);
+  .match(':tag.begin', ':tag?', ':tag.end', ':ws', passthrough);
 
 /**
  * Open tag.
  */
 
-expression('open-tag')
-  .match('<', ':ws', ':tag-name', ':ws', ':attr*', ':ws', '>', function($1, $2, $3, $4, $5, $6, $7){
-    var tokens = [];
-    tokens.push(token('punctuation', $1));
-    if ($1) tokens.push($2);
-    tokens.push($3);
-    if ($4) tokens.push($4);
-    if ($5) {
-      $5.forEach(function(x){
-        tokens = tokens.concat(x);
-      });
-    }
-    if ($6) tokens.push($6);
-    tokens.push(token('punctuation', $7));
-    return tokens;
-  });
+expression('tag.begin')
+  .match(
+    ':tag.punctuation.bracket.begin', 
+    ':ws', 
+    ':tag.name', 
+    ':ws', 
+    ':tag.punctuation.bracket.end', 
+    passthrough
+  );
+
+expression('tag.punctuation.bracket.begin')
+  .match('<', value);
+
+expression('tag.punctuation.bracket.end')
+  .match('>', value);
+
+expression('tag.punctuation.bracket.close.begin')
+  .match('</', value);
+
+expression('tag.punctuation.bracket.close.end')
+  .match('/>', value);
 
 /**
  * Tag name.
  */
 
-expression('tag-name')
-  .match(/[\w:-]+/, function($1){
-    return token('tag', $1);
-  });
+expression('tag.name')
+  .match(/[\w:-]+/, value);
 
 /**
  * Tag attribute.
  */
 
-expression('attr')
-  .match(':attr-name', ':ws', ':equal', ':ws', ':attr-value', ':ws', function($1, $2, $3, $4, $5, $6){
-    var tokens = [];
-    tokens.push(token('attr', $1));
-    if ($2) tokens.push($2);
-    if ($4) tokens.push($4);
-    tokens = tokens.concat($5);
-    if ($6) tokens.push($6);
-    return tokens;
-  });
+expression('tag.attribute')
+  .match(
+    ':tag.attribute.name', 
+    ':ws', 
+    ':tag.attribute.operator', 
+    ':ws', 
+    ':tag.attribute.value', 
+    ':ws', 
+    passthrough
+  );
 
 /**
  * Attribute name.
  */
 
-expression('attr-name')
+expression('tag.attribute.name')
   .match(/[\w:-]+/, value);
 
 /**
  * Attribute value.
  */
 
-expression('attr-value')
-  .match('"', ':attr-expression', '"', attr)
-  .match("'", ':attr-expression', "'", attr)
-  .match(':attr-expression', value);
+expression('tag.attribute.value')
+  .match(':string.quote.double', ':tag.attribute.value.expression', ':string.quote.double', passthrough)
+  .match(':string.quote.single', ':tag.attribute.value.expression', ':string.quote.single', passthrough)
+  .match(':tag.attribute.value.expression', passthrough);
 
 /**
- * Attribute expression.
+ * Double quotes.
  */
 
-expression('attr-expression')
-  .match(/[^<"'>]+/, function($1){
-    return token('expression', $1);
-  });
+expression('string.quote.double')
+  .match('"', value);
 
 /**
- * Close tag.
+ * Single quote.
  */
 
-expression('close-tag')
-  .match('<', ':ws', '/', ':ws', ':tag-name', ':ws', '>', function($1, $2, $3, $4, $5, $6, $7){
-    var tokens = [];
-    tokens.push(token('punctuation', $1));
-    if ($2) tokens.push($2);
-    tokens.push(token('punctuation', $3));
-    if ($4) tokens.push($4);
-    tokens.push($5);
-    if ($6) tokens.push($6);
-    tokens.push(token('punctuation', $7));
-    return tokens;
-  });
+expression('string.quote.single')
+  .match("'", value);
 
-/**
- * Self-closing tag.
- */
+// /**
+//  * Attribute expression.
+//  */
 
-expression('inline-tag')
-  .match('<', ':ws', ':attr-name', ':ws', '/', ':ws', '>');
+expression('tag.attribute.value.expression')
+  .match(/[^<"'>]+/, value);
 
-/**
- * Text element.
- */
+// /**
+//  * Close tag.
+//  */
 
-expression('text')
-  .match(/[^<>]+/, function($1){
-    return token('text', $1);
-  });
+expression('tag.end')
+  .match(
+    ':tag.punctuation.bracket.close.begin', 
+    ':ws', 
+    ':tag.name', 
+    ':ws', 
+    ':tag.punctuation.bracket.close.end', 
+    passthrough
+  );
 
-/**
- * Comment.
- */
+// /**
+//  * Self-closing tag.
+//  */
 
-expression('comment')
-  //.match('<!--', /[\w\W]*/, '-->', function($1, $2, $3){
-  // requires lookahead
-  .match('<!--', /[a-z\s]*/, '-->', function($1, $2, $3){
-    return token('comment', $1 + $2 + $3);
-  });
+// expression('inline-tag')
+//   .match('<', ':ws', ':attr-name', ':ws', '/', ':ws', '>');
 
-/**
- * Cdata.
- */
+// /**
+//  * Text element.
+//  */
 
-expression('cdata')
-  .match(/<!\[CDATA\[[\w\W]*?]]>/i, value);
+// expression('text')
+//   .match(/[^<>]+/, function($1){
+//     return token('text', $1);
+//   });
 
-/**
- * HTML entity.
- */
+// /**
+//  * Comment.
+//  */
 
-expression('entity')
-  .match(/\&#?[\da-z]{1,8};/i, value);
+// expression('comment')
+//   //.match('<!--', /[\w\W]*/, '-->', function($1, $2, $3){
+//   // requires lookahead
+//   .match('<!--', /[a-z\s]*/, '-->', function($1, $2, $3){
+//     return token('comment', $1 + $2 + $3);
+//   });
 
-expression('equal')
-  .match('=', function(val){
-    return token('operator', val);
-  });
+// /**
+//  * Cdata.
+//  */
 
-/**
- * Whitespace.
- */
+// expression('cdata')
+//   .match(/<!\[CDATA\[[\w\W]*?]]>/i, value);
+
+// /**
+//  * HTML entity.
+//  */
+
+// expression('entity')
+//   .match(/\&#?[\da-z]{1,8};/i, value);
+
+// expression('equal')
+//   .match('=', function(val){
+//     return token('operator', val);
+//   });
+
+// /**
+//  * Whitespace.
+//  */
 
 expression('ws')
-  .match(/[\s]*/, function(val){
-    if (val) return token('whitespace', val);
-    return val;
-  });
+  .match(/[\s]*/, value);
+
+function Token(type, content) {
+  this.type = type;
+  this.content = content;
+}
 
 function token(type, val) {
-  return [ type, val ];
+  //return [ type, val ];
+  return new Token(type, val);
 }
 
 function value(val) {
-  return val;
+  return token(this.expression.name, val);
 }
 
-function args() {
-  var x = Array.prototype.slice.call(arguments);
-  var r = [];
-  for (var i = 0, n = x.length; i < n; i++) {
-    r = r.concat(x[i]);
-  }
-  return r;
+function passthrough() {
+  return token(this.expression.name, slice.call(arguments));
 }
 
-function attr($1, $2, $3) {
-  return [
-    token('punctuation', $1),
-    $2,
-    token('punctuation', $3),
-  ];
-}
+// function values() {
+//   // for each
+//   return token(this.expression.name, val);
+// }
+
+// function args() {
+//   var x = Array.prototype.slice.call(arguments);
+//   var r = [];
+//   for (var i = 0, n = x.length; i < n; i++) {
+//     r = r.concat(x[i]);
+//   }
+//   return r;
+// }
+
+// function attr($1, $2, $3) {
+//   return [
+//     token('punctuation', $1),
+//     $2,
+//     token('punctuation', $3),
+//   ];
+// }
 
 function log() {
   console.log('log', arguments)
 }
+// expression('meta.tag.sgml.doctype.html')
+//   .match(':ws', /[a-z]+/, value);
+
+// expression('meta.tag.block.any.html')
+//   .match(
+//     ':punctuation.definition.tag.html',
+//     ':entity.name.tag.block.any.html',
+//     ':punctuation.definition.tag.end.html',
+//     value
+//   );
+
+// // address|blockquote|dd|div|section|article|aside|header|footer|nav|menu|dl|dt|fieldset|form|frame|frameset|h1|h2|h3|h4|h5|h6|iframe|noframes|object|ol|p|ul|applet|center|dir|hr|pre
+// expression('entity.name.tag.block.any.html')
+//   .match()
+
+// expression('meta.tag.any.html')
+//   .match(':')
+
+// expression('entity.name.tag.html')
+//   .match(/[a-zA-Z0-9:-]+/, value);
+
+// expression('entity.other.attribute-name.html')
+//   .match(/(?<=[^=])\b([a-zA-Z0-9:-]+)/)
+
+// expression('string.quoted.double.html')
+//   .match(
+//     ':punctuation.definition.string.begin.html',
+//     ':entities',
+//     ':punctuation.definition.string.end.html')
+
+// /**
+//  * Top of HTML file.
+//  */
+
+// expression('prolog')
+//   .match(/<\?.+?\?>/);
+
+// /**
+//  * Doctype.
+//  */
+
+// expression('meta.tag.sgml.html')
+//   .match(
+//     ':punctuation.definition.tag.html.open.first',
+//     ':entity.name.tag.doctype.html',
+//     ':meta.tag.sgml.doctype.html',
+//     ':punctuation.definition.tag.html.close',
+//     value
+//   );
+
+// expression('entity.name.tag.doctype.html')
+//   .match('doctype', value)
+//   .match('DOCTYPE', value);
+
+// expression('punctuation.definition.tag.html.open.first')
+//   .match('<!', value);
+// expression(':entities')
+//   .match(':constant.character.entity.html')
+
+// expression('constant.character.entity.html')
+//   .match(':punctuation.definition.entity.html')
+
+// expression('tag-stuff')
+//   .match(':tag-id-attribute', value)
+//   .match(':entity.other.attribute-name.html', value) //.match(':tag-generic-attribute', value)
+  
+//   '#tag-generic-attribute'
+//   'string.quoted.double.html', '#string-double-quoted'
+//   '#string-single-quoted'
+//   '#embedded-code'
+//   '#unquoted-attribute'
+
+// /**
+//  * Doctype tag name.
+//  */
+
+// expression('doctype-name')
+//   .match('doctype', value)
+//   .match('DOCTYPE', value);

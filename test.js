c82eb248f3608e3e559a43370e74279df71a84a5
Lance Pollard <lancejpollard@gmail.com>
2014-06-01 17:32:04 -0700

    make testing easier

--- test.js
+++ test.js
@@ -5,10 +5,47 @@ var Parser = require('grammarjs-recursive-parser');
 var parser = new Parser(grammar);
 
 describe('html', function(){
-  it('tag.attribute', function(){
-    var str = '<p id="content"></p><span></span>';
+  it('tag', function(){
+    var str = '<p></p>';
     var val = parser.parse(str);
-    
-    console.log(JSON.stringify(val, null, 2));
+    assert.equal(str, stringify(val));
+  });
+
+  describe('tag.attribute', function(){
+    it('id="content"', function(){
+      var str = '<p id="content"></p>';
+      var val = parser.parse(str);
+      assert.equal(str, stringify(val));
+    });
+
+    it('contenteditable', function(){
+      var str = '<p contenteditable></p>';
+      var val = parser.parse(str);
+      console.log(val);
+      assert.equal(str, stringify(val));
+    });
   });
 });
+
+/**
+ * For testing, it should generate the original string.
+ *
+ * @param {Token} token
+ * @api public
+ */
+
+function stringify(token) {
+  if (!token) return '';
+  var html = [];
+  if (Array.isArray(token.content)) {
+    token.content.forEach(function(child){
+      html.push(stringify(child));
+    });
+  } else if (null != token.content) {
+    html.push(stringify(token.content));
+  } else {
+    html.push(token);
+  }
+
+  return html.join('');
+}
